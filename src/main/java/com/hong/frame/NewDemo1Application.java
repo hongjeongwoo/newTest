c0bfcddd86859e3d14debb2e58133eb67e870dd1
prohong.pro <prohong.pro@gmail.com>
2021-04-28 04:10:42 +0900

    aa

---
+++ src/main/java/com/hong/frame/NewDemo1Application.java
@@ -0,0 +1,13 @@
+package com.hong.frame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewDemo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewDemo1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/hong/frame/NewDemo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hong.frame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewDemo1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
